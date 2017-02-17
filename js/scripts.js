184cd527180054da2cf843c917c6831bb16e2455
Mark <mark.aaron.fisher@gmail.com>
2017-02-17 10:39:01 -0800

    add jQuery functionality to change text of the two span tags in the results section and declare and explain the purpose of the assignSurveyResults function in the back end of scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+// Back End
+var assignSurveyResults = function(q2, q3, q4, q5, q6){
+  // Assigns the best track based on user's answers to the survey questions
+  // q2-q6 are the questions in the survey excluding the one asking the user to enter his/her/their name
+  // q2-q6 are assumed to be numbers at this point
+  // idea is to increment scores of the different track variables based on user responses, then report the max of those values at the end. If there's a tie, choose at random.
+  var Ruby = 0;
+  var PHP = 0;
+  var Java = 0;
+  var CSS = 0;
+  var Csharp = 0;
+
+
+}
+
+// Front End
 // console.log("js is working");
 $(function(){
   // console.log("doc ready is working");
@@ -11,9 +27,12 @@ $(function(){
     var q4response = parseInt($("input:radio[name=app-design]:checked").val());
     var q5response = parseInt($("input:radio[name=startup]:checked").val());
     var q6response = parseInt($("input:radio[name=google]:checked").val());
-    console.log(UsrName, q2response, q3response, q4response, q5response, q6response);
+    // console.log(UsrName, q2response, q3response, q4response, q5response, q6response);
+    var bestTrack =  assignSurveyResults(q2response, q3response, q4response, q5response, q6response);
     $("#results-section").show();
     $("#survey-section").hide();
+    $(".name").text(UsrName);
+    $(".track").text(bestTrack);
     event.preventDefault();
   });
   $("#results-reset-form").submit(function(){
